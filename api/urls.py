a3613dd4f5f86a456d382d73c451ddc468ff920a
sirvin01 <salimjr201@gmail.com>
2024-09-11 20:59:10 +0300

    v1

---
+++ api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path("",views.BlogPostListCreate.as_view(), name="blog-view-create"),
+    path("blogpost/",views.BlogPostListCreate.as_view(), name="blog-view-create"),
+    path("blogpost/<int:pk>/",views.BlogPostRetrieveUpdateDestroy.as_view(), name="blog-update")
+]
