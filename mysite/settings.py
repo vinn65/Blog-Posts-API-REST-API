ceaed7d77e96a34c5fd8c43bf2c37fe3d2c2c9bc
sirvin01 <salimjr201@gmail.com>
2024-09-25 21:04:46 +0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-i35=%=9v%t9(x*!v_r)!_^yl&av*-b*!p9lp%e5=kxy&-6nh#("
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['blog-posts-api-rest-api-t54y.vercel.app']
+
 
 
 # Application definition
