df8889b3c0862c5d0ea6508162d1cb35ad404947
sirvin01 <salimjr201@gmail.com>
2024-09-25 11:54:34 +0300

    dashboard

---
+++ dashboard/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import  path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
